2eeeb42fe53c6aa8bddbe1a55d4373d9fca0db72
Himanshu Dahiya <himanshu.44909@gmail.com>
2024-08-11 00:47:05 +0530

    env file update

--- db/connect.js
+++ db/connect.js
@@ -10,7 +10,7 @@
 // module.exports = connectDB;
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

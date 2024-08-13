2b890286e7e1094d3e4b624efc86fd2fceb20786
Himanshu Dahiya <himanshu.44909@gmail.com>
2024-08-13 21:04:14 +0530

    New Changes

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,7 +1,7 @@
 // controllers/chat.js
 
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({ port: 8080 });
 
 const admin = require('firebase-admin');
 const db = require('../db/connect'); // Path to your Firebase initialization file
@@ -58,34 +58,34 @@ const sendMessage = async (req, res) => {
 module.exports = { createChatRoom, sendMessage };
 
 // Store client connections by chat room
-const chatRoomClients = {};
+// const chatRoomClients = {};
 
-server.on('connection', (ws, req) => {
-    console.log('New client connected');
+// server.on('connection', (ws, req) => {
+//     console.log('New client connected');
 
-    // Extract chat room ID from query params
-    const chatRoomId = new URL(req.url, `http://${req.headers.host}`).searchParams.get('chatRoomId');
+//     // Extract chat room ID from query params
+//     const chatRoomId = new URL(req.url, `http://${req.headers.host}`).searchParams.get('chatRoomId');
 
-    if (!chatRoomClients[chatRoomId]) {
-        chatRoomClients[chatRoomId] = [];
-    }
-    chatRoomClients[chatRoomId].push(ws);
+//     if (!chatRoomClients[chatRoomId]) {
+//         chatRoomClients[chatRoomId] = [];
+//     }
+//     chatRoomClients[chatRoomId].push(ws);
 
-    // Listen for changes in Firestore for the specific chat room
-    db.collection('chatRooms').doc(chatRoomId).collection('messages')
-        .onSnapshot((snapshot) => {
-            snapshot.docChanges().forEach((change) => {
-                if (change.type === 'added') {
-                    chatRoomClients[chatRoomId].forEach(client => {
-                        client.send(JSON.stringify(change.doc.data()));
-                    });
-                }
-            });
-        });
+//     // Listen for changes in Firestore for the specific chat room
+//     db.collection('chatRooms').doc(chatRoomId).collection('messages')
+//         .onSnapshot((snapshot) => {
+//             snapshot.docChanges().forEach((change) => {
+//                 if (change.type === 'added') {
+//                     chatRoomClients[chatRoomId].forEach(client => {
+//                         client.send(JSON.stringify(change.doc.data()));
+//                     });
+//                 }
+//             });
+//         });
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        // Remove the client from the list
-        chatRoomClients[chatRoomId] = chatRoomClients[chatRoomId].filter(client => client !== ws);
-    });
-});
+//     ws.on('close', () => {
+//         console.log('Client disconnected');
+//         // Remove the client from the list
+//         chatRoomClients[chatRoomId] = chatRoomClients[chatRoomId].filter(client => client !== ws);
+//     });
+// });
